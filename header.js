7622cff62cc93d957e9def89638b215cfcfbdceb
Fredslund7889 <Fredslund7889@gmail.com>
2025-01-30 19:53:00 +0100

    done

---
+++ header.js
@@ -0,0 +1,9 @@
+document.write(`
+    <header>
+      <div>
+        <a href="index.html">Hjem</a>
+        <a href="guitar.html">Guitar</a>
+        <a href="opskrifter.html">Opskrifter</a>
+      </div>
+    </header>
+  `);
